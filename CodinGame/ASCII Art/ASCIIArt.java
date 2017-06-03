c01535b9a23069cbd91021036ec2e92fc7728081
Kieran McGregor <kieranmcgregor@gmail.com>
2017-06-02 23:32:12 -0400

    ASCII Art complete

--- CodinGame/ASCII Art/ASCIIArt.java
+++ CodinGame/ASCII Art/ASCIIArt.java
@@ -12,17 +12,40 @@ class Solution {
         Scanner in = new Scanner(System.in);
         int L = in.nextInt();
         int H = in.nextInt();
+
+        String[] ROWS = new String[H];
+        String printout;
+
         if (in.hasNextLine()) {
             in.nextLine();
         }
-        String T = in.nextLine();
-        for (int i = 0; i < H; i++) {
-            String ROW = in.nextLine();
+        String T = in.nextLine().toUpperCase();
+        char[] ch = T.toCharArray();
+
+        for (int i = 0; i < H; i++)
+        {
+            String NEWROW = in.nextLine();
+            ROWS[i] = NEWROW;
+            printout = "";
+
+            for (char c : ch)
+            {
+                String ROW = ROWS[i];
+                // Write an action using System.out.println()
+                // To debug: System.err.println("Debug messages...");
+                int temp = (int)c - 1;
+                int temp_integer = 64;
+                int position = temp - temp_integer;
+
+                if (position < 0 || position > 25)
+                {
+                    position = 26;
+                }
+
+                int start = ((position) * L);
+                printout += ROW.substring(start, (start + L));
+            }
+             System.out.println(printout);
         }
-
-        // Write an action using System.out.println()
-        // To debug: System.err.println("Debug messages...");
-
-        System.out.println("answer");
     }
 }
