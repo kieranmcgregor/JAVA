7fd7371fe787c8a8ce5c227cf479d4fd551c025a
Kieran McGregor <kieranmcgregor@gmail.com>
2017-06-06 21:15:52 -0400

    Finished MIME type problem and onto Defibrillators problem

--- CodinGame/MIME_Type/mimeType.java
+++ CodinGame/MIME_Type/mimeType.java
@@ -9,42 +9,36 @@ import java.math.*;
 class Solution {
 
     public static void main(String args[]) {
-        String mime_type = "UNKNOWN";
         Map<String, String> map = new HashMap<String, String>();
 
         Scanner in = new Scanner(System.in);
         int N = in.nextInt(); // Number of elements which make up the association table.
         int Q = in.nextInt(); // Number Q of file names to be analyzed.
-        for (int i = 0; i < N; i++) {
+        for (int i = 0; i < N; i++)
+        {
             String EXT = in.next(); // file extension
             String MT = in.next(); // MIME type.
 
-            System.err.println(EXT);
-            // System.err.println(MT);
-
-            map.put(EXT, MT);
-
-            // System.err.println(map.get(EXT));
+            map.put(EXT.toLowerCase(), MT);
         }
         in.nextLine();
-        for (int i = 0; i < Q; i++) {
+        for (int i = 0; i < Q; i++)
+        {
+            String mime_type = "UNKNOWN";
             String FNAME = in.nextLine(); // One file name per line.
-            System.err.println(FNAME);
 
-            int last_dot_i = FNAME.lastIndexOf(".");
-            int sub_start = last_dot_i + 1;
-            String file_ext = FNAME.substring(sub_start);
-
-            System.err.println(file_ext);
-            System.err.println(map.get(file_ext) != null);
-
-            if (map.get(file_ext) != null)
+            if (FNAME.lastIndexOf(".") != -1)
             {
-                mime_type = map.get(file_ext);
+                int last_dot_i = FNAME.lastIndexOf(".");
+                // System.err.println(last_dot_i + "\n");
+                int sub_start = last_dot_i + 1;
+                String file_ext = FNAME.substring(sub_start).toLowerCase();
+
+                if (map.get(file_ext) != null)
+                {
+                    mime_type = map.get(file_ext);
+                }
             }
-
-            System.err.println(mime_type);
-
             // For each of the Q filenames, display on a line the corresponding MIME type. If there is no corresponding type, then display UNKNOWN.
             System.out.println(mime_type);
         }
