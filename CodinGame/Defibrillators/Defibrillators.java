2ab869a653e48c901316280aa6417b70729b1200
Kieran McGregor <kieranmcgregor@gmail.com>
2017-06-11 20:54:51 -0400

    Defibrillators challenge done

--- CodinGame/Defibrillators/Defibrillators.java
+++ CodinGame/Defibrillators/Defibrillators.java
@@ -13,16 +13,39 @@ class Solution {
         String LON = in.next();
         String LAT = in.next();
         int N = in.nextInt();
+
+        float F_LON = Float.parseFloat(LON.replaceAll(",", "."));
+        float F_LAT = Float.parseFloat(LAT.replaceAll(",", "."));
+        String answer = "";
+        double shortest_dist = 0;
+
         if (in.hasNextLine()) {
             in.nextLine();
         }
         for (int i = 0; i < N; i++) {
             String DEFIB = in.nextLine();
+            String[] deets = DEFIB.split(";");
+
+            int pos = Integer.parseInt(deets[0]);
+
+            float defib_lon = Float.parseFloat(deets[4].replaceAll(",", "."));
+            float defib_lat = Float.parseFloat(deets[5].replaceAll(",", "."));
+
+            double x = Math.abs(F_LON - defib_lon);
+            double y = Math.abs(F_LAT - defib_lat);
+
+            double dist = Math.sqrt((x*x) + (y*y));
+
+            if (pos == 1 || dist < shortest_dist)
+            {
+                shortest_dist = dist;
+                answer = deets[1];
+            }
         }
 
         // Write an action using System.out.println()
         // To debug: System.err.println("Debug messages...");
 
-        System.out.println("answer");
+        System.out.println(answer);
     }
 }
